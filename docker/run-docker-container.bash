90340810bd5aefdabb10e7749ca5977dad345476
pytholic <rajahaseeb147@gmail.com>
2022-12-13 15:58:59 +0900

    modified run-docker-container.bash

--- docker/run-docker-container.bash
+++ docker/run-docker-container.bash
@@ -12,7 +12,21 @@ xhost +local:root
 # -e https_proxy=http://10.141.6.84:7890 \
 
 # run docker
-docker run --rm \
+docker run \
+  --ipc=host \
+  --gpus all \
+  --privileged \
+  -p 3751:22 \
+  -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:$docker/.Xauthority \
+  -v $HOME/work:/home/work/projects \
+  -v /mnt/Data/Datasets/dm-vio:/mnt/Data/Datasets/dm-vio \
+  -e XAUTHORITY=$home_folder/.Xauthority \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -it chengran222/tumtandem
+
+docker start \
   --ipc=host \
   --gpus all \
   --privileged \
