c9c516fa8acad3c9fbaf2104e3c542df2310b3a6
pytholic <rajahaseeb147@gmail.com>
2022-12-14 11:27:48 +0900

    updated dockerfile

--- docker/run-docker-container.bash
+++ docker/run-docker-container.bash
@@ -12,7 +12,7 @@ xhost +local:root
 # -e https_proxy=http://10.141.6.84:7890 \
 
 # run docker
-docker run --entrypoint "/bin/bash" \
+docker run \
   --ipc=host \
   --gpus all \
   --privileged \
@@ -25,17 +25,3 @@ docker run --entrypoint "/bin/bash" \
   -e DISPLAY=$DISPLAY \
   -e QT_X11_NO_MITSHM=1 \
   -it pytholic/tumtandem
-
-# docker start \
-#   --ipc=host \
-#   --gpus all \
-#   --privileged \
-#   -p 3751:22 \
-#   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-#   -v $HOME/.Xauthority:$docker/.Xauthority \
-#   -v $HOME/work:/home/work/projects \
-#   -v /mnt/Data/Datasets/dm-vio:/mnt/Data/Datasets/dm-vio \
-#   -e XAUTHORITY=$home_folder/.Xauthority \
-#   -e DISPLAY=$DISPLAY \
-#   -e QT_X11_NO_MITSHM=1 \
-#   -it pytholic/tumtandem
